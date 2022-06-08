379ddf4cc88ed9f9f692520225c151307d9072fb
thaian161 <thaian161@yahoo.com>
2022-06-08 16:30:06 -0500

    establish play.js

---
+++ play.js
@@ -0,0 +1,28 @@
+const net = require('net');
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243', // IP address here,
+    port: 50541, // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  //event handlers
+  conn.on('connect', () => {
+    // code that does something when the connection is first established
+    console.log('Welcome To The Jungle 🌿🐒🦜🐊🐘🌺!!!');
+  });
+
+  //Handling message from the server and console.log it out on terminal
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+console.log('Connecting ...');
+connect();
